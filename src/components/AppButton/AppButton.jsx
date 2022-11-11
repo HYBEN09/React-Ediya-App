54fda4677bd98004a8ecd92fa93f936028195090
hyebeen byun <hyebeen2658@naver.com>
2022-11-11 22:06:34 +0900

    컴포넌트 참조 전달(forwardRef)과 개발 도구에서 이름 표시 설정

--- src/components/AppButton/AppButton.jsx
+++ src/components/AppButton/AppButton.jsx
@@ -5,22 +5,23 @@ import React, { forwardRef } from "react";
 //? React.forwardRef는 props와 ref 파라미터를 받아 React 노드를 반환하는 렌더링 함수를 받습니다.
 // ref문서 : https://ko.reactjs.org/docs/forwarding-refs.html
 
-const AppButton = forwardRef(
-  ({ children, className, label, ...restProps }, ref) => {
-    const combineClassNames = classNames(["resetButton", className]);
-    return (
-      <button
-        ref={ref}
-        className={combineClassNames}
-        type="button"
-        {...restProps}
-        title={label || null}
-        aria-label={label || null}
-      >
-        {children}
-      </button>
-    );
-  }
-);
+const AppButton = ({ children, className, label, ...restProps }, ref) => {
+  const combineClassNames = classNames(["resetButton", className]);
+  return (
+    <button
+      ref={ref}
+      className={combineClassNames}
+      type="button"
+      {...restProps}
+      title={label || null}
+      aria-label={label || null}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default AppButton;
+//? forwardRef 이름 변경
+AppButton.displayName = "CommonButton";
+
+export default forwardRef(AppButton);

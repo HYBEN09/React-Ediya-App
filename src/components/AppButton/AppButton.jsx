92a0e826dce4041507977064d1b467c36d102e73
hyebeen byun <hyebeen2658@naver.com>
2022-11-11 22:02:43 +0900

    키보드 접근성 설정 (ref, forwardRef, shouldComponentUpdate)

--- src/components/AppButton/AppButton.jsx
+++ src/components/AppButton/AppButton.jsx
@@ -1,19 +1,26 @@
 import classNames from "classnames";
-import React from "react";
+import React, { forwardRef } from "react";
 
-const AppButton = ({ children, className, label, ...restProps }) => {
-  const combineClassNames = classNames(["resetButton", className]);
-  return (
-    <button
-      className={combineClassNames}
-      type="button"
-      {...restProps}
-      title={label || null}
-      aria-label={label || null}
-    >
-      {children}
-    </button>
-  );
-};
+//? React.forwardRef API를 사용하여 <AppNavigation/> 컴포넌트에 대한 refs를 명시적으로 전달할 수 있습니다.
+//? React.forwardRef는 props와 ref 파라미터를 받아 React 노드를 반환하는 렌더링 함수를 받습니다.
+// ref문서 : https://ko.reactjs.org/docs/forwarding-refs.html
+
+const AppButton = forwardRef(
+  ({ children, className, label, ...restProps }, ref) => {
+    const combineClassNames = classNames(["resetButton", className]);
+    return (
+      <button
+        ref={ref}
+        className={combineClassNames}
+        type="button"
+        {...restProps}
+        title={label || null}
+        aria-label={label || null}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 export default AppButton;
--- src/components/AppNavigation/AppNavigation.jsx
+++ src/components/AppNavigation/AppNavigation.jsx
@@ -1,9 +1,16 @@
 import AppButton from "components/AppButton/AppButton";
 import EdiyaContext from "context/ediyaContext";
-import { Fragment, Component } from "react";
+import { Fragment, Component, createRef } from "react";
 import "./AppNavigation.scss";
 
 class AppNavigation extends Component {
+  constructor(props) {
+    super(props);
+    this.refs = createRef();
+  }
+
+  //------------------------------------------------------------------------------------------------------
+
   static contextType = EdiyaContext;
 
   state = {
@@ -41,10 +48,52 @@ class AppNavigation extends Component {
     );
   };
 
+  handleFocusCloseMenuButton = (e) => {
+    //keyCode: 9 => tabKey
+    // 처음 메뉴탭에서 shift + Tab 눌렀다면 closeButton으로 이동.
+    if (e.shiftKey && e.keyCode === 9) {
+      e.preventDefault();
+      this.refs.closeButton.focus();
+    }
+  };
+
+  handleFocusFirstLink = (e) => {
+    // closeButton에서 Tab키를 눌렀을때 메뉴로 이동
+    if (!e.shiftKey && e.keyCode === 9) {
+      e.preventDefault();
+      this.refs.firstLink.focus();
+    }
+  };
+
+  // ? 렌더링된 다음 접근 위해
+  // componentDidMount() {
+  //   console.log(this.refs);
+  // }
+
+  handleEscCloseMenu = (e) => {
+    console.log("keydown");
+    // Esc 키를 눌렀을때 창 닫기. (Esc = keycode:27)
+    if (e.keyCode === 27) {
+      this.handleCloseMenu();
+    }
+  };
+
+  shouldComponentUpdate(nextProps, { isOpen }) {
+    const methodName = isOpen ? "addEventListener" : "removeEventListener";
+
+    // event를 추가 or 제거 (Tab키를 누르다가 Esc키를 누르고 다시 Tab키를 눌렀을때 'keydown'이 더 이상 연결되지않음)
+    window[methodName]("keydown", this.handleEscCloseMenu);
+
+    return true;
+  }
+
+  //------------------------------------------------------------------------------------------------------
+
   render() {
     const {
       navigation: { title, items },
     } = this.context;
+
     return (
       <Fragment>
         <AppButton
@@ -60,14 +109,25 @@ class AppNavigation extends Component {
           <ul className="resetList">
             {items.map(({ link, text }, index) => (
               <li key={`${link}-${index}`}>
-                <a href={link}>{text}</a>
+                <a
+                  ref={index === 0 ? "firstLink" : null}
+                  href={link}
+                  onKeyDown={
+                    // index가 첫번째가 되면 handleFocusCloseMenuButton을 실행하고 아니면 null
+                    index === 0 ? this.handleFocusCloseMenuButton : null
+                  }
+                >
+                  {text}
+                </a>
               </li>
             ))}
           </ul>
           <AppButton
+            ref="closeButton"
             className="is-close-menu"
             label="메뉴 닫기"
             onClick={this.handleCloseMenu}
+            onKeyDown={this.handleFocusFirstLink}
           >
             <span className="close" aria-hidden="true">
               x

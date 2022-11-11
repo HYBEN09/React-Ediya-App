67e321c43d9d939338692ceb13ec9b4fdef24fb8
hyebeen byun <hyebeen2658@naver.com>
2022-11-11 15:44:22 +0900

    이벤트 핸들링 & 타입 컨트롤

--- src/components/AppNavigation/AppNavigation.jsx
+++ src/components/AppNavigation/AppNavigation.jsx
@@ -1,52 +1,92 @@
-import { Fragment } from "react";
+import { Fragment, Component } from "react";
 import "./AppNavigation.scss";
 
-const AppNavigation = () => {
-  return (
-    <Fragment>
-      <button
-        className="resetButton is-open-menu"
-        type="button"
-        title="메뉴 열기"
-        aria-label="메뉴 열기"
-      >
-        <span className="ir"></span>
-      </button>
+class AppNavigation extends Component {
+  state = {
+    isOpen: false,
+    classes: "appNavigation",
+  };
 
-      <nav className="appNavigation" hidden>
-        <h2 className="a11y-hidden">메인 메뉴</h2>
-        <ul className="resetList">
-          <li>
-            <a href="#menu">메뉴</a>
-          </li>
-          <li>
-            <a href="#members">이디야멤버스</a>
-          </li>
-          <li>
-            <a href="#coffee-lab">이디야커피랩</a>
-          </li>
-          <li>
-            <a href="#culture-lab">이디야컬쳐랩</a>
-          </li>
-          <li>
-            <a href="#notice">공지사항</a>
-          </li>
-          <li>
-            <a href="#find-store">매장찾기</a>
-          </li>
-        </ul>
+  handleOpenMenu = (e) => {
+    this.setState(
+      {
+        isOpen: true,
+      },
+      () => {
+        window.setTimeout(() => {
+          this.setState({
+            classes: "appNavigation is-active",
+          });
+        }, 100);
+      }
+    );
+  };
+
+  handleCloseMenu = (e) => {
+    this.setState(
+      {
+        classes: "appNavigation",
+      },
+      () => {
+        window.setTimeout(() => {
+          this.setState({
+            isOpen: false,
+          });
+        }, 300);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <Fragment>
         <button
-          className="button is-close-menu"
+          className="resetButton is-open-menu"
           type="button"
-          title="메뉴 닫기"
-          aria-label="메뉴 닫기"
+          title="메뉴 열기"
+          aria-label="메뉴 열기"
+          onClick={this.handleOpenMenu}
         >
-          <span className="close" aria-hidden="true">
-            ×
-          </span>
+          <span className="ir"></span>
         </button>
-      </nav>
-    </Fragment>
-  );
-};
+
+        <nav className={this.state.classes} hidden={!this.state.isOpen}>
+          <h2 className="a11y-hidden">메인 메뉴</h2>
+          <ul className="resetList">
+            <li>
+              <a href="#menu">메뉴</a>
+            </li>
+            <li>
+              <a href="#members">이디야멤버스</a>
+            </li>
+            <li>
+              <a href="#coffee-lab">이디야커피랩</a>
+            </li>
+            <li>
+              <a href="#culture-lab">이디야컬쳐랩</a>
+            </li>
+            <li>
+              <a href="#notice">공지사항</a>
+            </li>
+            <li>
+              <a href="#find-store">매장찾기</a>
+            </li>
+          </ul>
+          <button
+            className="resetButton is-close-menu"
+            type="button"
+            title="메뉴 닫기"
+            aria-label="메뉴 닫기"
+            onClick={this.handleCloseMenu}
+          >
+            <span className="close" aria-hidden="true">
+              ×
+            </span>
+          </button>
+        </nav>
+      </Fragment>
+    );
+  }
+}
+
 export default AppNavigation;

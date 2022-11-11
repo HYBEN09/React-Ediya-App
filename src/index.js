fe0cd2d69a4d7556055e91a1eff170911a3e6439
hyebeen byun <hyebeen2658@naver.com>
2022-11-11 11:34:44 +0900

    ediya-ui-practice 폴더 생성

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
-import { StrictMode } from 'react'
-import { render } from 'react-dom'
-import './styles/index.scss'
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import * as ReactDOM from "react-dom/client";
+import "./styles/index.scss";
 
-import App from './app/App'
+import App from "./app/App";
 
-render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+if (process.env.NODE_ENV === "production") {
+  import("~/config/serviceWorker")
+    .then((serviceWorker) => serviceWorker.register)
+    .catch((error) => console.error(error.message));
+}

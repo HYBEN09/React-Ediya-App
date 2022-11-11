07dd90daae35ec21ce11796d8bae9554952d07d2
hyebeen byun <hyebeen2658@naver.com>
2022-11-12 01:06:51 +0900

    다이얼로그 인터랙션 (BeverageItem) -Hook 사용

--- src/components/BeverageItem/BeverageItem.jsx
+++ src/components/BeverageItem/BeverageItem.jsx
@@ -1,52 +1,81 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { Fragment, useState } from "react";
 import "./BeverageItem.scss";
 
-const BeverageItem = ({ item }) => {
-  console.log(item);
+const BeverageItem = ({
+  item: {
+    id,
+    detail: { desc, display_criteria: criteria, en, ko },
+    figure: { width, height, name, src },
+  },
+}) => {
+  //? Hook 사용
+  //눌린 상태 (pressed)
+  const [pressed, setPressed] = useState(false);
+
+  const [dialogClassName, setDialogClassName] = useState(
+    "beverageList__dialog"
+  );
+
+  const uniqueId = `ediya-menu__item-${id}`;
+
+  //* ----------------------------------------------------------------------------------------------
+
+  //? 성분 상세 정보 창 열기
+  function handleShowDialog(e) {
+    e.preventDefault();
+    setPressed(true);
+    window.setTimeout(() => {
+      setDialogClassName("beverageList__dialog is-active");
+    }, 100);
+  }
+
+  //? 성분 상세 정보 창 닫기
+  function handleHideDialog() {
+    setDialogClassName("beverageList__dialog");
+    setPressed(true);
+    window.setTimeout(() => {
+      setPressed(false);
+    }, 400);
+  }
+
+  //* ----------------------------------------------------------------------------------------------
+
   return (
     <li className="beverageList__item">
       <a
         href="#"
         role="button"
         aria-haspopup="true"
-        aria-pressed="false"
+        aria-pressed={pressed}
         className="beverageList__button"
+        onClick={handleShowDialog}
       >
         <figure>
-          <img
-            src="https://yamoo9.github.io/assets/images/ediya/iced-cherry-blossoms-latte.png"
-            width="323"
-            height="323"
-            alt=""
-          />
-          <figcaption>ICED 벚꽃라떼</figcaption>
+          <img src={src} width={width} height={height} alt="" />
+          <figcaption>{name}</figcaption>
         </figure>
       </a>
       <div
-        className="beverageList__dialog"
-        hidden
+        className={dialogClassName}
+        hidden={!pressed}
         role="dialog"
         aria-modal="false"
-        aria-labelledby="ediya-menu__item-1"
+        aria-labelledby={uniqueId}
       >
         <h3 className="beverageList__dialog--name" id="ediya-menu__item-1">
-          ICED 벚꽃라떼<span lang="en">Cherry Blossom Latte</span>
+          {ko}
+          <span lang="en">{en}</span>
         </h3>
+        <p>{desc}</p>
         <div className="beverageList__dialog--multiColumn is-2">
           <dl>
-            <dt>칼로리</dt>
-            <dd>(393kcal)</dd>
-            <dt>당류</dt>
-            <dd>(35g)</dd>
-            <dt>단백질</dt>
-            <dd>(7g)</dd>
-            <dt>포화지방</dt>
-            <dd>(18.6g)</dd>
-            <dt>나트륨</dt>
-            <dd>(149mg)</dd>
-            <dt>카페인</dt>
-            <dd>(0mg)</dd>
+            {criteria.map(([dt, dd], index) => (
+              <Fragment key={index}>
+                <dt>{dt}</dt>
+                <dd>{dd}</dd>
+              </Fragment>
+            ))}
           </dl>
         </div>
         <button
@@ -54,6 +83,7 @@ const BeverageItem = ({ item }) => {
           type="button"
           title="닫기"
           aria-label="음료 정보 패널 닫기"
+          onClick={handleHideDialog}
         >
           <span aria-hidden="true">×</span>
         </button>
